88bd228344b67ce02784edd82f3deb694da3b42c
Manpreet Verma <manubroo112@gmail.com>
2026-05-27 18:36:02 +0530

    Add Vercel Analytics component

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./styles.css";
 import { registerServiceWorker } from "./utils/pwa";
-import { Analytics } from '@vercel/analytics/react';
 
 registerServiceWorker();
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+      <Analytics />
   </React.StrictMode>
 );
